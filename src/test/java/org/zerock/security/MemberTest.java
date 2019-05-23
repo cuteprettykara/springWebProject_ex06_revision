c24993456c3f0708b6f379b89f54769d1b753d3d
cuteprettykara <cuteprettykara@gmail.com>
2019-05-23 16:56:03 +0900

    testInsertAuth 주석 처리

--- src/test/java/org/zerock/security/MemberTest.java
+++ src/test/java/org/zerock/security/MemberTest.java
@@ -1,5 +1,7 @@
 package org.zerock.security;
 
+import static org.junit.Assert.*;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -80,51 +82,56 @@ public class MemberTest {
 //		}
 //	}
 	
+//	@Test
+//	public void testInsertAuth() {
+//		String sql = "insert into tbl_member_auth(userid, auth) values(?,?)";
+//		
+//		Connection con = null;
+//		PreparedStatement pstmt = null;
+//		
+//		try {
+//			con = ds.getConnection();
+//			pstmt = con.prepareStatement(sql);
+//			
+//			for (int i = 0; i < 100; i++) {
+//				if (i < 80) {
+//					pstmt.setString(1, "user" + i);
+//					pstmt.setString(2, "ROLE_USER");
+//				} else if (i < 90) {
+//					pstmt.setString(1, "manager" + i);
+//					pstmt.setString(2, "ROLE_MANAGER");
+//				} else {
+//					pstmt.setString(1, "admin" + i);
+//					pstmt.setString(2, "ROLE_ADMIN");
+//				}
+//				
+//				pstmt.executeUpdate();
+//			}
+//			
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//			
+//		} finally {
+//			if (pstmt != null) {
+//				try {
+//					pstmt.close();
+//				} catch (SQLException e) {
+//					e.printStackTrace();
+//				}
+//			}
+//			
+//			if (con != null) {
+//				try {
+//					con.close();
+//				} catch (SQLException e) {
+//					e.printStackTrace();
+//				}
+//			}
+//		}
+//	}
+	
 	@Test
-	public void testInsertAuth() {
-		String sql = "insert into tbl_member_auth(userid, auth) values(?,?)";
-		
-		Connection con = null;
-		PreparedStatement pstmt = null;
-		
-		try {
-			con = ds.getConnection();
-			pstmt = con.prepareStatement(sql);
-			
-			for (int i = 0; i < 100; i++) {
-				if (i < 80) {
-					pstmt.setString(1, "user" + i);
-					pstmt.setString(2, "ROLE_USER");
-				} else if (i < 90) {
-					pstmt.setString(1, "manager" + i);
-					pstmt.setString(2, "ROLE_MANAGER");
-				} else {
-					pstmt.setString(1, "admin" + i);
-					pstmt.setString(2, "ROLE_ADMIN");
-				}
-				
-				pstmt.executeUpdate();
-			}
-			
-		} catch (SQLException e) {
-			e.printStackTrace();
-			
-		} finally {
-			if (pstmt != null) {
-				try {
-					pstmt.close();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-			}
-			
-			if (con != null) {
-				try {
-					con.close();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-			}
-		}
+	public void testNull() throws Exception {
+		log.info("Test passed!!!");
 	}
 }
