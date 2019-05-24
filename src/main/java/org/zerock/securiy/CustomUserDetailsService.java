2279451e99cf7f77118897760d784acef24fb4a9
cuteprettykara <cuteprettykara@gmail.com>
2019-05-24 12:06:47 +0900

    Part7 chapter 33. - Spring Web Security를 이용한 로그인 처리 - 커스텀 UserDetailsService 활용(MemberVO를 UserDetails 타입으로 변환)

--- src/main/java/org/zerock/securiy/CustomUserDetailsService.java
+++ src/main/java/org/zerock/securiy/CustomUserDetailsService.java
@@ -1,20 +1,32 @@
 package org.zerock.securiy;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.zerock.domain.MemberVO;
+import org.zerock.mapper.MemberMapper;
+import org.zerock.securiy.domain.CustomUser;
 
+import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
 @Log4j
 public class CustomUserDetailsService implements UserDetailsService{
+	
+	@Setter(onMethod_ = @Autowired)
+	private MemberMapper memberMapper;
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
 		log.warn("Load User By UserName : " + username);
 		
-		return null;
+		MemberVO vo = memberMapper.read(username);	// username means userid
+		
+		log.warn("queried by member mapper : " + vo);
+		
+		return vo == null ? null : new CustomUser(vo);
 	}
 
 }
