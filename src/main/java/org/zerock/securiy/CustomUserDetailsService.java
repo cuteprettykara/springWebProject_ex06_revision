40f411e2bc49464f7dae5a99ad7d72f3d134b097
cuteprettykara <cuteprettykara@gmail.com>
2019-05-24 11:55:42 +0900

    Part7 chapter 33. - Spring Web Security를 이용한 로그인 처리 - 커스텀 UserDetailsService 활용

---
+++ src/main/java/org/zerock/securiy/CustomUserDetailsService.java
@@ -0,0 +1,20 @@
+package org.zerock.securiy;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+public class CustomUserDetailsService implements UserDetailsService{
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		log.warn("Load User By UserName : " + username);
+		
+		return null;
+	}
+
+}
